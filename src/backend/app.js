75f5f43d5359ad9351adaa49184b6df1e3aef5f3
lucasolbr <lucasolibran@gmail.com>
2026-06-21 18:25:26 -0300

    Update app.js foto perfil

--- src/backend/app.js
+++ src/backend/app.js
@@ -10,6 +10,9 @@ app.use(express.json());
 // Servir arquivos estáticos do frontend
 app.use(express.static(path.join(__dirname, '../frontend')));
 
+// Servir as fotos de perfil enviadas pelos usuários
+app.use('/uploads/profile-pictures', express.static(path.join(__dirname, 'uploads/profile-pictures')));
+
 const routes = require('./routes');
 app.use('/', routes);
 
