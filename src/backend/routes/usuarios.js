e8a6df070b5239bab5d715ec3f8158e669aed31f
lucasolbr <lucasolibran@gmail.com>
2026-06-21 18:24:17 -0300

    Update usuarios.js foto perfil

--- src/backend/routes/usuarios.js
+++ src/backend/routes/usuarios.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
 const { autenticar } = require('../middlewares/auth');
+const { processarUploadFoto } = require('../middlewares/upload');
 
 // Rota para obter dados públicos do perfil
 router.get('/:id', autenticar, usuarioController.buscarPerfil);
@@ -11,5 +12,9 @@ router.put('/:id/bio', autenticar, usuarioController.atualizarBio);
 
 // Rota para obter estatísticas de treinos
 router.get('/:id/training-stats', autenticar, usuarioController.obterEstatisticasTreinos);
+
+// Rotas para gerenciar a foto de perfil
+router.post('/:id/profile-picture', autenticar, processarUploadFoto, usuarioController.uploadFotoPerfil);
+router.delete('/:id/profile-picture', autenticar, usuarioController.removerFotoPerfil);
 
 module.exports = router;
