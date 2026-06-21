65f0b672e7723871970d704448be5467281d7fdb
lucasolbr <lucasolibran@gmail.com>
2026-06-21 15:47:52 -0300

    Update usuarios.js
    feat: add routes for profile view and bio update

--- src/backend/routes/usuarios.js
+++ src/backend/routes/usuarios.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
 const { autenticar } = require('../middlewares/auth');
 
+// Rota para obter dados públicos do perfil
+router.get('/:id', autenticar, usuarioController.buscarPerfil);
+
+// Rota para editar biografia e nível de corrida
+router.put('/:id/bio', autenticar, usuarioController.atualizarBio);
+
 // Rota para obter estatísticas de treinos
 router.get('/:id/training-stats', autenticar, usuarioController.obterEstatisticasTreinos);
 
